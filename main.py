cc31fa6b7e081e667facf0bd22d70daed4719cab
Ryan Ward <ryan@rwtech.io>
2024-07-13 12:51:17 -0500

    Initial display logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
+from bs4 import BeautifulSoup
+import pyautogui
 
 CLIENT_ID = "1a35d17e2a7747d8a4901cca2f5af892"
 CLIENT_SECRET = "02cd43644d4e4e728b6bdc6834f86a5c"
@@ -13,45 +15,56 @@ currSong = None
 prevSong = None
 deviceId = None
 
+def display_song(song):
+  if song == None:
+    return
+
+  with open("index.html") as fp:
+    # Update web page with new song info
+    soup = BeautifulSoup(fp, features="html.parser")
+    soup.img["src"] = song["art"]
+    soup.find("div", {"class": "bg-image"})["style"] = "background-image: url('" + song["art"] + "')" 
+    soup.h1.string.replace_with(song["title"])
+    soup.h2.string.replace_with(song["artist"])
+    new_text = soup.prettify()
+            
+    with open("index.html", mode='w') as new_html_file:
+        new_html_file.write(new_text)
+  
+  pyautogui.hotkey('command', 'r')
+
 def change_state(state, device):
     # If either state or device is null exit
     if state == None or device == None:
         return
-
-    if state == "play":
-        sp.start_playback(device_id=device)
-        print("Play")
-    elif state == "pause":
-        sp.pause_playback(device_id=device)
-        print("Pause")
-    elif state == "next":
-        sp.next_track(device_id=device)
-        print("Next")
-    elif state == "previous":
-        sp.previous_track(device_id=device)
-        print("Previous")
-    else:
-        print("Unknown state")
+    
+    try:
+        if state == "play":
+            sp.start_playback(device_id=device)
+        elif state == "pause":
+            sp.pause_playback(device_id=device)
+        elif state == "next":
+            sp.next_track(device_id=device)
+        elif state == "previous":
+            sp.previous_track(device_id=device)
+        else:
+            print("Unknown state")
+    except Exception as e:
+      print("Playback is already in current state: " + state)
+    
 
 def main():
     global currSong
     global prevSong
     global deviceId
     
+    # Get current song and playback info from Spotify
     song = sp.current_user_playing_track()
     playback = sp.current_playback()
 
     # Check if playback info was returned
     if playback != None:
         deviceId = playback['device']['id']
-        change_state("play", deviceId)
-        time.sleep(2)
-        change_state("pause", deviceId)
-        time.sleep(2)
-        change_state("next", deviceId)
-        time.sleep(2)
-        change_state("previous", deviceId)
-        time.sleep(2)
     
     # Check if song info was returned
     if song != None:
@@ -61,16 +74,9 @@ def main():
     if (currSong != prevSong):
         prevSong = currSong
         albumArt = song["item"]["album"]["images"][0]["url"]
+        artist = song["item"]["artists"][0]["name"]
+        display_song({"title": currSong, "art": albumArt, "artist": artist})
         print(currSong)
-        print(albumArt)
-
-    #device = playback['device']['id']
-    #time.sleep(5)
-    #sp.next_track(device_id=device)
-    #time.sleep(5)
-    #sp.previous_track(device_id=device)
-    #time.sleep(5)
-    #sp.volume(volume_percent=50,device_id=device)
 
 
 if __name__=="__main__":
